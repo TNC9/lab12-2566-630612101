cc4a37029756b74753021000cd0c24d8c162548f
TNC9 <koonbenzkub@hotmail.com>
2023-08-03 15:49:20 +0700

    Finish

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,11 @@ export default function HomePage() {
           <Controller />
         </LangContext.Provider>
       </ThemeContext.Provider>
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+      <Footer
+        year="2023"
+        fullName="Thananchai Chaimanee"
+        studentId="630612101"
+      />
     </div>
   );
 }
